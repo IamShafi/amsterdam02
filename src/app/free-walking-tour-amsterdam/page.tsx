8a6f93f7cfe6090a9b8288220b0f8432569559da
shafi <salekin68@gmail.com>
2025-11-19 15:53:36 +0600

    feat: server side above the fold

--- src/app/free-walking-tour-amsterdam/page.tsx
+++ src/app/free-walking-tour-amsterdam/page.tsx
@@ -1,10 +1,18 @@
-import TourDetails from "@/components/TourDetails";
-import React from "react";
+import Header from "@/components/Header";
+import ImageCarousel from "@/components/ImageCarousel";
+// import TourDetails from "@/components/TourDetails";
+
 
 const TourDetailsPage = () => {
   return (
-    <div>
-      <TourDetails />
+    <div className="min-h-[100svh] min-h-[calc(var(--vh,1vh)*100)] bg-background pb-24 lg:pb-0">
+      <Header />
+      {/* Hero Carousel */}
+      <div className="overflow-hidden">
+        {/* use default images */}
+        <ImageCarousel />
+      </div>
+      {/* <TourDetails /> */}
     </div>
   );
 };
